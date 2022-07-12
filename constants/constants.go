b116f86bdc13f06da494c23c058394ea6c241808
jiaqicheng <jiaqicheng@ufl.edu>
2022-07-11 23:33:21 -0600

    init repo

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const (
+	USER_INDEX  = "user"
+	POST_INDEX  = "post"
+	ES_URL      = "http://10.128.0.2:9200"
+	ES_USERNAME = "lightyagmai"
+	ES_PASSWORD = "12345678"
+	GCS_BUCKET  = "lightyagmai_bucket"
+)
